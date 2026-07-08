fed38bf924a5bb90f42d08781287cb3cf0f46ed0
unknown <nobody@nowhere.com>
2026-07-08 21:49:00 +0800

    Fix duplicate BRep import in lib.rs

--- libs/rcad-algorithms/src/lib.rs
+++ libs/rcad-algorithms/src/lib.rs
@@ -296,7 +296,6 @@ pub use top_loc::{
 };
 
 use rcad_kernel::BRep;
-use rcad_kernel::BRep;
 use rcad_kernel::topods;
 
 // pub use adaptor3d::{Curve3dAdaptor, CurveOnSurfaceAdaptor, HSurfaceAdaptor, SurfaceAdaptor}; // removed
